56021ad8a188438553a09d4be26adb83ed24cf67
KatherineMayen88 <70446775+KatherineMayen88@users.noreply.github.com>
2023-02-19 23:53:51 -0600

    Delete ED1_Practica2 directory

--- ED1_Practica2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ED1_Practica2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
