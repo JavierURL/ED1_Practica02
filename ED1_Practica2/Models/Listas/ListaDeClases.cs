9fe8104c0d9d3652c5a74280b84d227a62116d60
JlopezV <J0274nj@gmail.com>
2023-02-19 21:37:51 -0600

    creacion de la lista de clases

---
+++ ED1_Practica2/Models/Listas/ListaDeClases.cs
@@ -0,0 +1,25 @@
+﻿using ED1_Practica2.Models;
+
+namespace ED1_Practica2.Listas
+{
+	public class JugadoresYEquipos
+	{
+
+		public List<Jugadores> RetornarJugadores()
+		{
+			List<Jugadores> ListaDeJugadores = new List<Jugadores>();
+
+			return ListaDeJugadores;
+		}
+
+
+		public List<Equipo> RetornarEquipo()
+		{
+			List<Equipo> ListaDeEquipos = new List<Equipo>();
+
+			return ListaDeEquipos;
+		}
+
+	}
+}
+}
